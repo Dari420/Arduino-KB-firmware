b9585b7e315d671561fcfec8d4f57c1b9476e4b2
Dari <Dari>
2020-01-25 14:31:02 -0500

    added a configuration file

--- Code.ino
+++ Code.ino
@@ -1,36 +1,12 @@
 /*
-  Made by Dari#3459 on discord
-  Keyboard firmware for atmega32u4  based boards
+  	Made by Dari#3459 on discord
+  	Keyboard firmware for HID capable arduino boards
 */
 
 #include <HID-Project.h>
 #include <Keypad.h>
 #include <FastLED.h>
-
-//Defining LED values
-#define LED_TYPE WS2812B //Put the type of LED you're using
-#define NUM_LEDS 60 //Put the amount of LED's you have here
-#define LED_PIN 6 //Pin LED is going to connect to
-CRGB leds[NUM_LEDS]; //initializing a block of memory to 
-//choose your colour values for first solid colour
-int RED_0 = 201;
-int BLUE_0 = 50;
-int GREEN_0 = 179;
-
-//Defining pins/values, self explanatory
-const int debounce = 200 //ms of debounce time, change to whatever
-const byte ROWS = 2; //Rows, horizontal, edit for your own use
-const byte COLS = 3; //Columns, vertical, edit for your own use
-const int pinLed = 10; //capslock
-const int pinLed2 = 14; //numlock
-const int pinLed3 = 15; //scrolllock
-byte rowPins[ROWS] = {9, 8}; //pin arrays
-byte colPins[COLS] = {5, 4, 3};
-const int message_strings = 6; //enter the amount of string entries you have 
-const int functions = 6; //enter the amount of functions you have
-bool LED_DelayState = false; //check if led clock is on or off, don't touch unless you know what you're doing.
-long interval = 30; //how long we are going to be delaying the led clock
-long previousMillis = 0; //previous millis value used for async functions
+#include <config.h>
 
 /* to change your keymap, edit this. valid numbers are 1-(amount of strings) for text, and (amount of strings + amount of functions) for functions. 
 Simply follow the formulas to find which number you want to put. Function/String number can be found in the arrays. For functions, formula is 6 + function number
@@ -41,7 +17,39 @@ char keymap[ROWS][COLS] { //Rows (Y) by Columns (X)
   	{'4', '5', '6'}
 };
 
-//to change your functions and messages, edit these. Just follow the formatting
+/*
+	Pins for your matrix, add/edit them here. You may also remove them.
+	Need help? https://www.arduino.cc/reference/en/language/variables/data-types/array/
+*/
+byte rowPins[ROWS] = {9, 8}; 
+byte colPins[COLS] = {5, 4, 3};
+
+//LED cycle
+CRGB leds[NUM_LEDS]; //initializing a block of memory to store the LEDS
+bool LED_DelayState = false; //check if led clock is on or off, don't touch unless you know what you're doing.
+long previousMillis = 0; //previous millis value used for async functions
+	
+
+/*
+								NOT CONFIGURABLE, SKIP
+	***************************************************************************************************
+	customkey intilization. Skip this unless you know what you're doing 
+*/
+//Making the button array
+Keypad customKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, ROWS, COLS);
+
+//Making customkey the result of what the keypad library gets
+int customKey = customKeypad.getKey();
+
+/*								NOT CONFIGURABLE ^ (unless you know what you're doing, then go ahead :^) )
+	****************************************************************************************************
+
+
+
+/*
+	To change your function and message macros, edit these. Just follow the formatting.
+	Need help? https://www.arduino.cc/en/Reference/KeyboardModifiers and https://github.com/NicoHood/HID/wiki/Keyboard-API
+*/
 
 //Text strings
 String messages[] = {
@@ -88,41 +96,42 @@ void RecordStop(){
   	Keyboard.press(KEY_F21);
   	Keyboard.releaseAll();
 }
-// Function array, order your functions here, not super useful but if you need it it's here. Read the keymap for a better explanation
+/*
+	Don't forget to include the function here!
+	Function array, order your functions here so you can use them in the keymap above
+*/
 void (*funcMacros[])() {
   	sleep, PsFill, VolumeUp, VolumeDown, Record, RecordStop
 };
 
-// anything after this is more technical, feel free to read
-
-//Making the button array
-Keypad customKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, ROWS, COLS);
-
-//Making customkey the result of what the keypad library gets
-int customKey = customKeypad.getKey();
-
-//Main macro 
-void Macro(int i) {
-  	if (i <= message_strings) {
- 		Keyboard.print(messages[i - 1]); //minus 1 because arrays are zero-indexed
-  	}
-  	else if (i <= (message_strings + functions)) { //output is under or equivalent to the maxmimum amount of valid entries (functions + strings), so that there's no memory errors.
-    	funcMacros[i - (message_strings + 1)]();  /*Subtracting away the string numbers so that it correctly finds the corresponding function in the array funcMacros. 
-                                          Additionally subtracing one more than the number of strings
-                                          because arrays are 0 indexed
-										 */
-  }
-}
+/*
+	LED functions. Don't forget to include them in the main loop! 
+*/
 
 void setLedSolidColour(){
   	fill_solid(LEDS, NUM_LEDS, CRGB(RED_0, GREEN_0, BLUE_0))
   	FastLED.show(); 
 }
 
+// anything after this is more technical, feel free to read
+
+//Main macro 
+void Macro(int i) {
+  	if (i <= Message_strings) {
+ 		Keyboard.print(messages[i - 1]); //minus 1 because arrays are zero-indexed
+  	}
+  	else if (i <= (Message_strings + Functions)) { //output is under or equivalent to the maxmimum amount of valid entries (functions + strings), so that there's no memory errors.
+    	funcMacros[i - (Message_strings + 1)](); 	/*Subtracting away the string numbers so that it correctly finds the corresponding function in the array funcMacros. 
+                                          		Additionally subtracing one more than the number of strings
+                                          		because arrays are 0 indexed
+										 */
+  }
+}
+
 void setup() {
-  	pinMode(pinLed, OUTPUT);
-  	pinMode(pinLed2, OUTPUT);
-  	pinMode(pinLed3, OUTPUT);
+  	pinMode(CapsLED, OUTPUT);
+  	pinMode(NumLED, OUTPUT);
+  	pinMode(ScrollLED, OUTPUT);
   	Keyboard.begin(); //Initialize libraries
   	BootKeyboard.begin();
   	FastLED.addLeds<LED_TYPE, DATA_PIN>(LEDS, NUM_LEDS);
@@ -130,16 +139,16 @@ void setup() {
 
 
 void loop() {
-  //led indecators
+  //led indicators
   	if (BootKeyboard.getLeds()) {
-    	digitalWrite(pinLed, (LED_CAPS_LOCK) ? HIGH : LOW); //basically, do something with a pin, here's the pin,                                                   check this variable (true, HIGH, false/else, LOW)
-    	digitalWrite(pinLed2, (LED_NUM_LOCK) ? HIGH : LOW);
-    	digitalWrite(pinLed3, (LED_SCROLL_LOCK) ? HIGH : LOW);
+    	digitalWrite(CapsLED, (LED_CAPS_LOCK) ? HIGH : LOW); //basically, do something with a pin, here's the pin,                                                   check this variable (true, HIGH, false/else, LOW)
+    	digitalWrite(NumLED, (LED_NUM_LOCK) ? HIGH : LOW);
+    	digitalWrite(ScrollLED, (LED_SCROLL_LOCK) ? HIGH : LOW);
   	}
 
   	if (customKey) {
     	Macro(customKey); //calls macro function with i being what customkey gives
-    	delay(debounce); //debounce
+    	delay(Debounce); //debounce5
   	}
   	/*async led writes
   	essentially every millisecond it'll check when 30 milliseconds have passed since the last bool state change (first if statment).
@@ -150,11 +159,13 @@ void loop() {
   	
   	if(currentMillis - previousMillis > interval) {
   		previousMillis = currentMillis; //Save last clock cycle
+
   		if(!LED_DelayState){
   			LED_DelayState = !LED_DelayState; //if false, set true
   		}
   		else {
   			LED_DelayState = !LED_DelayState; //if true, set false and set the LEDs
+  			//put your solid led colour functions here
   			setLedSolidColour();
   		}
   	}
---
+++ Config.h
@@ -0,0 +1,27 @@
+/*
+Configuration for the firmware, should be self explanatory for the most part
+*/
+
+//LEDs
+#define LED_TYPE WS2812B //Put the type of LED you're using
+#define NUM_LEDS 60 //Put the amount of LED's you have here
+#define LED_PIN 6 //Pin LED is going to connect to
+#define interval 30 //half of the time you want between each led refresh cycle
+
+//Colours, zero-indexed
+#define RED_0 201
+#define BLUE_0 50
+#define GREEN_0 279
+
+//Macros
+#define Message_strings 6 //enter the amount of string entries you have 
+#define Functions 6 //enter the amount of functions you have
+
+//Keyswitch definitions
+#define Debounce 200 //ms of debounce time, change to whatever
+#define ROWS 2 //Rows, horizontal, edit for your own use
+#define COLS 3 //Columns, vertical, edit for your own use
+#define CapsLED 10 //Pin for capslock LED
+#define NumLED 14 //Pin for numlock LED
+#define ScrollLED 15 //Pin for scrolllock LED
+
